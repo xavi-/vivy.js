69e1cc0209c9c2af669b8f4aa2d5f04a8688275a
Xavi Ramirez <xavi.rmz@gmail.com>
2025-12-20 00:56:27 -0600

    Made testing harness more consistent

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,20 +1,20 @@
 /** biome-ignore-all lint/correctness/noUnusedVariables: false positive */
-async function runTest(name, fn) {
+async function runTest(name, elem, fn) {
 	try {
 		console.log(`RUNNING: ${name}`);
-		const container = await fn();
+		await fn(elem);
 		console.log(`PASS: ${name}`);
-		const target = container instanceof HTMLElement ? container : document.body;
-		target.insertAdjacentHTML(
+		elem.insertAdjacentHTML(
 			"beforeend",
 			`<div style="color:green; font-weight:bold; margin: 4px 0; display: block;">PASS: ${name}</div>`,
 		);
 	} catch (e) {
-		console.error(`FAIL: ${name}`, e);
-		document.body.insertAdjacentHTML(
+		console.log(`FAIL: ${name}`, e);
+		elem.insertAdjacentHTML(
 			"beforeend",
 			`<div style="color:red; font-weight:bold; margin: 4px 0; display: block;">FAIL: ${name} - ${e.message}</div>`,
 		);
+		document.body.style.backgroundColor = "#fff0f0";
 	}
 }
 
